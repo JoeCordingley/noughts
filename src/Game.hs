d14b1b90109892213993ff54ce4cd6f21478d7af
Joe Cordingley <joewcordingley@gmail.com>
2024-11-30 01:18:12 +0000

    fun with lenses

--- src/Game.hs
+++ src/Game.hs
@@ -1,72 +1,65 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Game
-  ( Player (..),
+module Game (
+    Player (..),
     Game,
     GameStatus,
     gameStatus,
     nextPlayer,
     startingGame,
-    nw,
-    n,
-    ne,
-    w,
-    c,
-    e,
-    sw,
-    s,
-    se,
     Move (..),
     boardLens,
-  )
+)
 where
 
 import Control.Lens
-import Data.Maybe (fromMaybe)
+import Data.Semigroup (Any (..))
 
-data Player = Nought | Cross
+data Player = Nought | Cross deriving (Eq)
 
 type Game = Board (Maybe Player)
 
-type BoardLens = forall a. Lens (Board a) (Board a) a a
+type BoardLens a = Lens' (Board a) a
 
-data Board a = Board
-  { _ne :: a,
-    _n :: a,
-    _nw :: a,
-    _w :: a,
-    _c :: a,
-    _e :: a,
-    _sw :: a,
-    _s :: a,
-    _se :: a
-  }
+data Board a = Board {_t :: Line a, _m :: Line a, _b :: Line a}
 
+instance Functor Board where
+    fmap f (Board x y z) = Board (fmap f x) (fmap f y) (fmap f z)
+
+instance Applicative Board where
+    pure x = Board (pure x) (pure x) (pure x)
+    Board fx fy fz <*> Board x y z = Board (fx <*> x) (fy <*> y) (fz <*> z)
+
+instance Semigroup a => Semigroup (Board a) where
+    Board x1 y1 z1 <> Board x2 y2 z2 = Board (x1 <> x2) (y1 <> y2) (z1 <> z2)
+
+data Line a = Line {_l :: a, _c :: a, _r :: a}
+
+instance Functor Line where
+    fmap f (Line x y z) = Line (f x) (f y) (f z)
+
+instance Applicative Line where
+    pure x = Line x x x
+    Line fx fy fz <*> Line x y z = Line (fx x) (fy y) (fz z)
+
+instance Semigroup a => Semigroup (Line a) where
+    Line x1 y1 z1 <> Line x2 y2 z2 = Line (x1 <> x2) (y1 <> y2) (z1 <> z2)
+
+makeLenses ''Line
 makeLenses ''Board
 
 startingGame :: Game
-startingGame =
-  Board
-    { _ne = Nothing,
-      _n = Nothing,
-      _nw = Nothing,
-      _w = Nothing,
-      _c = Nothing,
-      _e = Nothing,
-      _sw = Nothing,
-      _s = Nothing,
-      _se = Nothing
-    }
+startingGame = pure Nothing
 
 winningLines :: [[Move]]
 winningLines =
-  [ [NW, N, NE],
-    [W, C, E],
-    [SW, S, SE],
-    [NW, C, SE],
-    [NE, C, SW]
-  ]
+    [ [NW, N, NE]
+    , [W, C, E]
+    , [SW, S, SE]
+    , [NW, C, SE]
+    , [NE, C, SW]
+    ]
 
 nextPlayer :: Player -> Player
 nextPlayer Nought = Cross
@@ -74,34 +67,42 @@ nextPlayer Cross = Nought
 
 data Move = NW | N | NE | W | C | E | SW | S | SE
 
-boardLens :: Move -> BoardLens
+boardLens :: Move -> BoardLens a
 boardLens move = case move of
-  NW -> nw
-  N -> n
-  NE -> ne
-  W -> w
-  C -> c
-  E -> e
-  SW -> sw
-  S -> s
-  SE -> se
+    NW -> t . l
+    N -> t . c
+    NE -> t . r
+    W -> m . l
+    C -> m . c
+    E -> m . r
+    SW -> b . l
+    S -> b . c
+    SE -> b . r
 
 type GameStatus = Either FinishedGame Game
 
 type FinishedGame = Board FinishedSquare
 
-data FinishedSquare = FinishedSquare {winningSquare :: Bool, mark :: Maybe Player}
+data FinishedSquare = FinishedSquare Bool (Maybe Player)
+
+type WinningSquares = Board Bool
 
 gameStatus :: Player -> Game -> GameStatus
-gameStatus player game = case wonGame player game of
-  Just finished -> Left finished
-  Nothing -> Right game
-
-wonGame :: Player -> Game -> Maybe FinishedGame
-wonGame player game = join <$> foldr f Nothing winningLines
+gameStatus player game = case wonGame of
+    Just winningSquares -> Left $ FinishedSquare <$> winningSquares <*> game
+    Nothing -> Right game
   where
-    f line maybeFinished = maybe (tryLine empty) tryLine maybeFinished
+    wonGame :: Maybe WinningSquares
+    wonGame =
+        fmap2 getAny $
+            foldMap (fmap2 Any . tryLine) winningLines
       where
-        tryLine = undefined
-    empty = undefined
-    join = undefined
+        fmap2 = fmap . fmap
+        tryLine :: [Move] -> Maybe WinningSquares
+        tryLine = foldr ((=<<) . applyMove) (Just $ pure False)
+          where
+            applyMove :: Move -> WinningSquares -> Maybe WinningSquares
+            applyMove move wonBoard =
+                if view (boardLens move) game == Just player
+                    then Just $ set (boardLens move) True wonBoard
+                    else Nothing
